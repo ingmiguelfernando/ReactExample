197056eea91020c99d61c498c55752a54bbb2aae
ing.miguel.fernando <ing.miguel.fernando@gmail.com>
2020-05-25 17:02:58 -0500

    Redux store Design

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,6 +1,41 @@
+import _ from "lodash";
 import jsonPlaceHolder from "../apis/jsonPlaceHolder";
 
-export const fetchPosts = () => async dispatch => {
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+  
+  //const userIds = _.uniq(_.map(getState().posts, "userId"));
+  //userIds.forEach(id => dispatch(fetchUser(id)));
+ 
+  _.chain(getState().posts)
+    .map("userId")
+    .uniq()
+    .forEach(id => dispatch(fetchUser(id)))
+    .value()
+  
+};
+
+export const fetchPosts = () => async (dispatch) => {
   const response = await jsonPlaceHolder.get("/posts");
   dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
+
+export const fetchUser = (id) => async (dispatch) => {
+  const response = await jsonPlaceHolder.get(`/users/${id}`);
+  dispatch({ type: "FETCH_USER", payload: response.data });
+};
+
+//one request each one
+
+//export const fetchUser = (id) => async dispatch => {
+//  const response = await jsonPlaceHolder.get(`/users/${id}`);
+//  dispatch({type: "FETCH_USER", payload:response.data})
+//}
+
+// cache the same request
+// export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch);
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceHolder.get(`/users/${id}`);
+//   dispatch({ type: "FETCH_USER", payload: response.data });
+// });
