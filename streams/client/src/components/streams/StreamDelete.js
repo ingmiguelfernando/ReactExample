9a7004abc59b5020dd66474165ed16aa0b67470e
ing.miguel.fernando <ing.miguel.fernando@gmail.com>
2020-06-10 21:51:11 -0500

    Using React Portals Unit

--- streams/client/src/components/streams/StreamDelete.js
+++ streams/client/src/components/streams/StreamDelete.js
@@ -1,11 +1,57 @@
-import React from 'react'
+import React from "react";
+import Modal from "../Modal";
+import history from "../../history";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { fetchStream, deleteStream } from "../../actions";
 
-const StreamDelete = () => {
+class StreamDelete extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  DeleteStream() {
+    this.props.deleteStream(this.props.match.params.id);    
+  }
+  RenderActions() {
     return (
-        <div>
-            StreamDelete
-        </div>
-    )
+      <div>
+        <button
+          onClick={() => this.DeleteStream()}
+          className="ui button negative"
+        >
+          Delete
+        </button>
+        <Link to="/" className="ui button">
+          Cancel
+        </Link>
+      </div>
+    );
+  }
+
+  RenderContent() {
+    if (!this.props.stream) {
+      return "Are you sure you want to delete this stream ?";
+    }
+    return `Are you sure you want to delete this stream with id ${this.props.stream.id}?`;
+  }
+
+  render() {
+    return (
+      <Modal
+        title="Delete Stream"
+        content={this.RenderContent()}
+        actions={this.RenderActions()}
+        onDismiss={() => history.push("/")}
+      />
+    );
+  }
 }
 
-export default StreamDelete;
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
+};
+
+export default connect(mapStateToProps, { fetchStream, deleteStream })(
+  StreamDelete
+);
--- streams/client/src/components/streams/StreamList.js
+++ streams/client/src/components/streams/StreamList.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 import { fetchStreams } from "../../actions";
 
 class StreamList extends React.Component {
@@ -11,8 +11,15 @@ class StreamList extends React.Component {
     if (stream.userId === this.props.currentUserId) {
       return (
         <div className="right floated content">
-          <Link to={`/streams/edit/${stream.id}`} className="ui button primary">Edit</Link>
-          <button className="ui button negative">Delete</button>
+          <Link to={`/streams/edit/${stream.id}`} className="ui button primary">
+            Edit
+          </Link>
+          <Link
+            to={`/streams/delete/${stream.id}`}
+            className="ui button negative"
+          >
+            Delete
+          </Link>
         </div>
       );
     }
@@ -31,17 +38,16 @@ class StreamList extends React.Component {
       );
     });
   }
-  renderCreate(){
-      if(this.props.isSignedIn){
-          return (
-              <div style={{textAlign: 'right'}}>
-                  <Link to="/streams/new"  className="ui primary button">
-                      Create Stream
-                  </Link>
-              </div>
-          )
-      }
-
+  renderCreate() {
+    if (this.props.isSignedIn) {
+      return (
+        <div style={{ textAlign: "right" }}>
+          <Link to="/streams/new" className="ui primary button">
+            Create Stream
+          </Link>
+        </div>
+      );
+    }
   }
   render() {
     return (
