392262049806a09581dabbe4dc0094b05a61b41b
ing.miguel.fernando <ing.miguel.fernando@gmail.com>
2021-01-02 18:16:52 -0500

    Manual React Router

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -3,6 +3,8 @@ import Accordion from "./components/Accordion";
 import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
 import Translate from "./components/Translate";
+import Route from "./components/Route";
+import Header from "./components/Header";
 
 const items = [
   {
@@ -30,14 +32,23 @@ export default () => {
   return (
     <div>
       <br />
-      {/* <Accordion items ={items}/> */}
-      {/* <Search/> */}
-      {/* <Dropdown
-        options={options}
-        selected={selected}
-        onSelectedChange={setSelected}
-      /> */}
-      <Translate />
+      <Header />
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/search">
+        <Search />
+      </Route>
+      <Route path="/dropdown">
+        <Dropdown
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+        />
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
     </div>
   );
 };
